f6e4a3c8b46fab73f6f81fbb3bc1386d109135a2
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-11 02:40:52 +0300

    Style decrease scale on hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ alt, ...props }: ButtonProps) => (
   <button
     {...props}
     className={twMerge(
-      `font-medium hover:scale-[1.1] focus:scale-[1.05] shadow-lg justify-center flex items-center bg-black/90 text-white gap-2 active:scale-[0.95] transition-colors transition-transform will-change-transform px-8 py-2 border rounded-lg border-black/90`,
+      `font-medium hover:scale-[1.05] focus:scale-[1.05] shadow-md hover:shadow-lg  justify-center flex items-center bg-black/90 text-white gap-2 active:scale-[0.95] transition-all transition-transform will-change-[transform,_shadow] px-8 py-2 border rounded-lg border-black/90`,
       alt && "bg-transparent text-black hover:bg-black/5",
       props.className
     )}
