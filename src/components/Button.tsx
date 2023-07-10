d149a6c5dd22c82824a42df8c2000f85b763ed40
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-10 20:46:10 +0300

    Update Button component to accept native button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,19 @@
-import { PropsWithChildren } from "react";
+import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends PropsWithChildren {
+interface ButtonProps extends ComponentProps<"button"> {
   alt?: boolean;
-  className?: string;
 }
 
-const Button = ({ alt, className, children }: ButtonProps) => (
+const Button = ({ alt, ...props }: ButtonProps) => (
   <button
+    {...props}
     className={twMerge(
-      `font-medium hover:scale-[1.1] shadow-lg flex items-center bg-black/90 text-white gap-2 active:scale-[0.9] transition-colors transition-transform will-change-transform px-8 py-2 border rounded-lg border-black/90`,
+      `font-medium hover:scale-[1.1] focus:scale-[1.05] shadow-lg justify-center flex items-center bg-black/90 text-white gap-2 active:scale-[0.95] transition-colors transition-transform will-change-transform px-8 py-2 border rounded-lg border-black/90`,
       alt && "bg-transparent text-black hover:bg-black/5",
-      className
+      props.className
     )}
-  >
-    {children}
-  </button>
+  />
 );
 
 export default Button;
