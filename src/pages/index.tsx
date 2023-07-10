37a1187b9afdc4ac8489a74486e9b326c656e833
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-10 20:30:40 +0300

    Update add Form and footer sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { FormEvent, useRef } from "react";
+
 import Header from "@/components/Header";
 import Button from "@/components/Button";
+import Form from "@/components/Form";
+
+import inputs from "../inputs";
+const intialUserInfo: { [key: string]: string } = {};
+inputs.map(input => {
+  intialUserInfo[input.name] = "";
+});
 
 export default function Home() {
+  const userInfo = useRef(intialUserInfo);
+
+  const generateCoverLetter = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(userInfo.current);
+  };
+
+  const focusOnFirstInput = () => {
+    document.getElementsByTagName("input")?.[0]?.focus();
+  };
+
   return (
     <main className={`min-h-screen max-w-5xl mx-auto`}>
       <Header />
@@ -9,9 +29,33 @@ export default function Home() {
         <h2 className="text-[min(10vw,3.5rem)] mt-16 font-bold text-center max-w-lg min-[900px]:max-w-3xl mx-auto">
           Generate Cover Letter Email Easly With GPT Model
         </h2>
-        <Button className="mx-auto mt-16">Try it now 👇</Button>
+        <Button className="mx-auto my-16" onClick={focusOnFirstInput}>
+          Try it now 👇
+        </Button>
+        <Form onSubmit={generateCoverLetter} userInfo={userInfo} />
       </section>
-      <footer></footer>
+      <footer className="px-4 font-medium py-6 text-slate-700 flex justify-between items-center border-t-2 border-black/5">
+        <p>
+          Created By{" "}
+          <a
+            target="_blank"
+            className="front-bold hover:underline"
+            href="https://github.com/mohamedalosaili/cover-letter-generator"
+          >
+            @MohammedAlosaili
+          </a>
+        </p>
+        <p>
+          Inspired By{" "}
+          <a
+            target="_blank"
+            className="front-bold hover:underline"
+            href="https://github.com/Nutlope/twitterbio"
+          >
+            twitterbio
+          </a>
+        </p>
+      </footer>
     </main>
   );
 }
