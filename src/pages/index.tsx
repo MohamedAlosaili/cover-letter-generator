b2aa6b93be83995a25fca5a8037c5eda78173883
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-11 16:39:31 +0300

    Add generatedSection & generate() submitHandler to send prompt to openAI API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,70 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 import Header from "@/components/Header";
 import Button from "@/components/Button";
 import Form from "@/components/Form";
 
-import inputs from "../inputs";
-const intialUserInfo: { [key: string]: string } = {};
-inputs.map(input => {
-  intialUserInfo[input.name] = "";
-});
+import inputs, { UserInfo } from "../inputs";
+import { toast } from "react-hot-toast";
+
+const intialUserInfo: UserInfo = Object.assign(
+  {},
+  ...inputs.map(input => ({ [input.name]: "" }))
+);
 
 export default function Home() {
-  const userInfo = useRef(intialUserInfo);
+  const userInfo = useRef<UserInfo>(intialUserInfo);
+  const [loading, setLoading] = useState(false);
+  const [generatedCoverLetter, setGeneratedCoverLetter] = useState("");
+  const gereratedSection = useRef<HTMLDivElement>(null);
 
-  const generateCoverLetter = (e: FormEvent) => {
+  const generate = async (e: FormEvent) => {
     e.preventDefault();
-    console.log(userInfo.current);
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInfo: userInfo.current }),
+      });
+
+      const result = (await response.json()) as {
+        success: boolean;
+        data?: any;
+        error?: string;
+      };
+      console.log(result);
+      if (!result.success) {
+        throw new Error(result.error);
+      }
+
+      setGeneratedCoverLetter(result.data.choices[0].message.content);
+      gereratedSection.current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    } catch (err) {
+      console.log(err);
+      toast.error("Failed to Generate", { icon: "😥" });
+    }
+    setLoading(false);
   };
 
   const focusOnFirstInput = () => {
     const input = document.getElementsByTagName("input")?.[0];
     if (input) {
       input?.focus();
-      input.scrollIntoView({ behavior: "smooth" });
+      input.scrollIntoView({ behavior: "smooth", block: "center" });
     }
+  };
+
+  const saveToClipboard = () => {
+    if (loading) return;
+    navigator.clipboard.writeText(generatedCoverLetter);
+    toast.success("Copied to clipboard");
   };
 
   return (
@@ -36,9 +77,30 @@ export default function Home() {
         <Button className="mx-auto my-16" onClick={focusOnFirstInput}>
           Try it now 👇
         </Button>
-        <Form onSubmit={generateCoverLetter} userInfo={userInfo} />
       </section>
-      <footer className="px-4 mt-16 font-medium py-6 text-slate-700 flex justify-between items-center border-t-2 border-black/5">
+      <Form onSubmit={generate} loading={loading} userInfo={userInfo} />
+      <div ref={gereratedSection}></div>
+      {generatedCoverLetter && (
+        <section className="max-w-3xl mx-auto p-4">
+          <h2 className="mt-4 pb-8 text-[min(7vw,2.5rem)] font-bold text-center">
+            Generated Cover Letter
+          </h2>
+          <div
+            className="relative group bg-slate-50 p-4 shadow-lg rounded-lg border border-black/25 cursor-pointer hover:bg-white transition-colors"
+            onClick={saveToClipboard}
+          >
+            <span className="absolute font-medium bottom-full left-1/2 -translate-x-1/2 translate-y-4 -z-10 transition-all group-hover:-translate-y-1 will-change-transform group-hover:opacity-100 opacity-0 pointer-events-none">
+              Click To Copy 📋
+            </span>
+            {generatedCoverLetter
+              .split("\n")
+              .map((text, idx) =>
+                text === "" ? <br key={idx} /> : <p key={idx}>{text}</p>
+              )}
+          </div>
+        </section>
+      )}
+      <footer className="px-4 mt-16 text-sm sm:text-base font-medium py-6 text-slate-700 flex justify-between gap-2 items-center border-t-2 border-black/5">
         <p>
           Created By{" "}
           <a
@@ -46,7 +108,7 @@ export default function Home() {
             className="front-bold hover:underline"
             href="https://github.com/mohamedalosaili/cover-letter-generator"
           >
-            @MohammedAlosaili
+            @Mohammed
           </a>
         </p>
         <p>
