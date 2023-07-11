987ee1ab2aa6d43da254d4ac0cba5c20eae171b9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-11 16:34:31 +0300

    Add UserInfo interface & Update input labels

--- src/inputs.ts
+++ src/inputs.ts
@@ -9,13 +9,24 @@ interface input {
   required: boolean;
 }
 
+export interface UserInfo {
+  candidateFullName: string;
+  jobTitle: string;
+  companyName: string;
+  releventSkills: string;
+  achievementsOrExperience: string;
+  hiringManagerName?: string;
+  whyThisRole?: string;
+  additionalInfo?: string;
+}
+
 const inputs: input[] = [
   {
     id: "1",
     name: "candidateFullName",
     inputType: "input",
     placeholder: "Your full name e.g. Mohammed Ali",
-    label: "Candidate Full Name",
+    label: "Candidate's Full Name:",
     hint: null,
     maxLength: 50,
     required: true,
@@ -25,7 +36,7 @@ const inputs: input[] = [
     name: "jobTitle",
     inputType: "input",
     placeholder: "Position or Job title e.g. Frontend Developer",
-    label: "Target Job Title",
+    label: "Position Being Applied For:",
     hint: null,
     maxLength: 50,
     required: true,
@@ -35,7 +46,7 @@ const inputs: input[] = [
     name: "companyName",
     inputType: "input",
     placeholder: "e.g. Google",
-    label: "Company Name",
+    label: "Company Name:",
     hint: "The name of the company or organization you applying to. This allows you to personalize the cover letter.",
     maxLength: 50,
     required: true,
@@ -45,7 +56,7 @@ const inputs: input[] = [
     name: "releventSkills",
     inputType: "input",
     placeholder: "Briefly mention your key skills",
-    label: "Relevant skills",
+    label: "Key Skills Relevant to the Job:",
     hint: null,
     maxLength: 100,
     required: true,
@@ -54,8 +65,8 @@ const inputs: input[] = [
     id: "5",
     name: "achievementsOrExperience",
     inputType: "textarea",
-    placeholder: "Briefly mention some of your Experience or Achievements.",
-    label: "Experience / Achievements",
+    placeholder: "Briefly mention notable Achievements or relevant Experience.",
+    label: "Achievements or Experience:",
     hint: "Mention any notable achievements or experiences relevant to the job, that would make you stand out as a candidate.",
     maxLength: 160,
     required: true,
@@ -65,7 +76,7 @@ const inputs: input[] = [
     name: "hiringManagerName",
     inputType: "input",
     placeholder: "Hiring Manager Name e.g. Khalid Ahmed",
-    label: "Recipient Name",
+    label: "Recipient's Name:",
     hint: "This can help to personalize the greeting. However, a generic greeting can be used if it's not known.",
     maxLength: 50,
     required: false,
@@ -75,7 +86,7 @@ const inputs: input[] = [
     name: "whyThisRole",
     inputType: "input",
     placeholder: "Why you're Interested in the Role/Company",
-    label: "Why you are Interest",
+    label: "Reasons for Interest in Role or Company:",
     hint: "This shows enthusiasm and can help you stand out as a candidate.",
     maxLength: 75,
     required: false,
@@ -85,7 +96,7 @@ const inputs: input[] = [
     name: "additionalInfo",
     inputType: "textarea",
     placeholder: "Any additional information or instruction",
-    label: "Addition information",
+    label: "Additional Information or Instructions:",
     hint: "Add anything you want that is not mentioned in the above fields",
     maxLength: 160,
     required: false,
