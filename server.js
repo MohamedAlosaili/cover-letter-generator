e033011855058a43505a403610cbedab1d373e7a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-22 18:10:44 +0300

    Add view engine and handle not found error with 404 status

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ import { config } from "dotenv";
 config();
 
 import express from "express";
+import inputs from "./inputs.js";
+import errorHandler from "./errorHandler.js";
 
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { inputs });
+});
 
 app.post("/api/generate", (req, res) => {
   console.log(req.body);
@@ -14,5 +21,21 @@ app.post("/api/generate", (req, res) => {
   res.send("Ok");
 });
 
+app.use("*", (req, res, next) => {
+  req.status = 404;
+  req.message = "Not found";
+
+  next(new Error());
+});
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on PORT ${PORT}`)
+);
+
+process.on("unhandledRejection", error => {
+  console.log(`${error.stack}`.red);
+
+  server.close(() => process.exit(1));
+});
