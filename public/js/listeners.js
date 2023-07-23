ffa59a725528d2562bcbe5460bd221827bb0e8e7
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-23 03:32:59 +0300

    Add listeners to focus on the first input, display range value, update input length, and show optional fields

---
+++ public/js/listeners.js
@@ -0,0 +1,38 @@
+const focusOnFirestInputBtn = document.querySelector("[data-focus-btn]");
+// Focus on the first input when clicking on 'Try it now 👇` Button
+focusOnFirestInputBtn.addEventListener("click", () => {
+  const input = document.querySelector("input[type='text']");
+  if (input) {
+    input.focus();
+    input.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+});
+
+// Range input
+const rangeInput = document.querySelector("[data-input='range']");
+const rangeValue = document.querySelector("[data-range-value]");
+// Display new range value on changes
+rangeInput.addEventListener("input", e => {
+  const { value, min } = e.target;
+
+  rangeValue.innerText = value === min ? "Default" : value;
+});
+
+// Set listener to update fields value length
+export const formInputs = document.querySelectorAll("[data-input]");
+formInputs.forEach(input => {
+  if (input.type === "range") return;
+  input.addEventListener("input", e => {
+    const inputLengthElement = document.querySelector(
+      `[data-maxlength-name="${input.name}"]`
+    );
+    inputLengthElement.innerText = `${input.value.length} / ${inputLengthElement.dataset.maxlength}`;
+  });
+});
+
+// Show and hide optional fields
+const optionalTrigger = document.querySelector("[data-optional-trigger]");
+const optionalInputs = document.querySelectorAll("[data-optional-input]");
+optionalTrigger.addEventListener("click", () => {
+  optionalInputs.forEach(input => input.classList.toggle("show"));
+});
