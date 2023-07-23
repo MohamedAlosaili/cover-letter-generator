b2535fcfc68233de1c80c2c494f18ce86838c64b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-07-23 03:33:22 +0300

    Add form submit event listener and process input values

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import { formInputs } from "./listeners.js";
+
+const form = document.querySelector("[data-form]");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const inputsValue = Array.from(formInputs).map(input => ({
+    [input.name]: input.value,
+  }));
+  const userInfo = Object.assign({}, ...inputsValue);
+  userInfo.tempreture = Number(userInfo.tempreture || 5) / 10;
+});
